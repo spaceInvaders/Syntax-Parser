30bfbaf906afe9242074db135fb35bb7887cd077
spaceInvader <sviat.gotsko@gmail.com>
2020-07-13 16:48:08 +0300

    Login Identity implemented

--- WebAppCalcMVC/Controllers/AccountController.cs
+++ WebAppCalcMVC/Controllers/AccountController.cs
@@ -21,11 +21,15 @@ namespace WebAppCalcMVC.Controllers
             _signInManager = signInManager;
         }
 
+
+        // Register
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
 
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
@@ -54,6 +58,53 @@ namespace WebAppCalcMVC.Controllers
             }
 
             return View(model);
+        }
+
+        // Login
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    // check if the URL belongs to the application
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username and / or password");
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            // delete authentication cookies
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
